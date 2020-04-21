12cb1bcaa21f0d9a7047ab0a6009b473870408b0
GIGOTech <tompreciousekwere@outlook.com>
2020-04-21 19:46:17 +0100

    Added JCalculator in the dev branch

---
+++ src/jcalculator.java
@@ -0,0 +1,18 @@
+public class jcalculator(){
+
+	public double getDifference(double a, double b){
+		return a-b;
+	}
+
+	public double getQuotient(double a, double b){
+		return a/b;
+	}
+
+	public double getProduct(double a, double b){
+		return a*b;
+	}
+
+	public double getSum(double a, double b){
+		return a+b;
+	}
+}
